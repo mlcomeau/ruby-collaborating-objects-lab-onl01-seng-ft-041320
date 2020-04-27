cdcfad03606b70cdee6be8b198d82be5582d10fd
Megan Comeau <mlcomeau99@gmail.com>
2020-04-27 21:37:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,11 +4,11 @@ class Song
 
   def initialize(name)
     @name = name
-    @@all << self 
+    @@all << self
   end
 
   def self.all
-    @@all 
+    @@all
   end
 
   def self.new_by_filename(filename)
