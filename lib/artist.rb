62a025b82cc7c12c530d951bb254953f57696c15
Megan Comeau <mlcomeau99@gmail.com>
2020-04-27 21:34:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,14 +22,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(artist_name)
-    found_artist = self.all.find {|artist| artist.name == artist_name}
-    if found_artist
-      found_artist
-    else
-      new_artist = self.new(artist_name)
-      new_artist.save
-      new_artist
-    end
+    self.all.detect {|artist| artist.name == artist_name} || Artist.new(artist_name)
   end
 
   def print_songs
