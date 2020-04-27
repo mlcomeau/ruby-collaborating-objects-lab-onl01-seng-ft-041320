067693d70b9aa206d66072de468e0ef892246a97
Megan Comeau <mlcomeau99@gmail.com>
2020-04-27 21:39:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,18 +11,12 @@ class Song
     @@all
   end
 
-  def self.new_by_filename(filename)
-    song = self.new
-    song.title = filename.split(" - ")[1]
-    song
+  def self.new_by_filename(song_name)
+    
   end
 
   def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
+
   end
 
 end
