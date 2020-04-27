e3d526bf11988c2081277d12ed0582dcb36a76c3
Megan Comeau <mlcomeau99@gmail.com>
2020-04-27 21:48:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
   def self.new_by_filename(filename)
     song_data = filename.split(" - ")
     song = Song.new(song_data[1])
-    song.artist = song_data[0]
+    song.artist.name = song_data[0]
     song
   end
 
