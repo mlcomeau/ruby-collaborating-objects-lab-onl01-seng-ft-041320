7a3365585b96fd23d31f38615dd2fad3bae93861
Megan Comeau <mlcomeau99@gmail.com>
2020-04-27 20:14:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,2 +1,31 @@
 class Artist
+  attr_accessor :name 
+  def initialize(name)
+    @name = name 
+  end
+
+  def self.all
+    Artist.all
+  end
+  
+  def add_song(song)
+    song.artist = self 
+  end
+
+  def songs 
+    song.all.select {|song| song.artist == self}
+  end
+
+  def self.find_or_create_by_name
+    if (self.artist.nil?)
+      self.artist = Artist.new(self)
+    else 
+      self.artist.name 
+    end
+  end
+
+  def print_songs 
+  end
+  
+  
 end
