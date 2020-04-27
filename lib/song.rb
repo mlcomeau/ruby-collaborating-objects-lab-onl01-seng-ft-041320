4e300da0d9f053fcd64bb9076c69812537769346
Megan Comeau <mlcomeau99@gmail.com>
2020-04-27 20:21:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,26 +1,26 @@
 class Song
-  attr_accessor :name, :artist 
+  attr_accessor :name, :artist
 
   def initialize(name)
-    @name = name 
+    @name = name
   end
 
-  def self.all 
+  def self.all
     Song.all
   end
 
   def self.new_by_filename(filename)
-    song = self.new 
+    song = self.new
     song.title = filename.split(" - ")[1]
-    song 
+    song
   end
 
   def artist_name=(name)
     if (self.artist.nil?)
       self.artist = Artist.new(name)
     else
-      self.artist.name = name 
+      self.artist.name = name
     end
   end
-  
+
 end
